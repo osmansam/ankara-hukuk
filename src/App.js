d3f4cdf9154f962751f238eceec9e6b821fff31a
Osman <114867365+osmansam@users.noreply.github.com>
2023-03-01 11:31:11 -0500

    add-info added into App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import AddBar from "./pages/AddBar";
+import AddInfo from "./pages/AddInfo";
 import Home from "./pages/Home";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -17,6 +18,9 @@ function App() {
         </Route>
         <Route path="/add-bar" exact>
           <AddBar />
+        </Route>
+        <Route path="/add-info" exact>
+          <AddInfo />
         </Route>
       </Switch>
       <ToastContainer position="top-center" />
