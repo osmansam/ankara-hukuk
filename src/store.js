268420a6d81db812273f48fe457935890985ade7
Osman <114867365+osmansam@users.noreply.github.com>
2023-03-01 11:11:54 -0500

    info slice and info thunk created

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./features/user/userSlice";
 import barSlice from "./features/bar/barSlice";
+import infoSlice from "./features/info/infoSlice";
 export const store = configureStore({
   reducer: {
     user: userSlice,
     bar: barSlice,
+    info: infoSlice,
   },
 });
