a8a0cf28f8d02dc3cec56871f5b0138a35d08e55
Osman <114867365+osmansam@users.noreply.github.com>
2023-03-07 10:55:46 -0500

    createHaber function is set

--- src/features/haber/haberSlice.js
+++ src/features/haber/haberSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
+import { checkForUnauthorizedResponse } from "../../utils/axios";
 import axios from "axios";
 
 const initialState = {
@@ -13,12 +14,44 @@ const initialState = {
   image: "",
   date: "",
 };
+//create haber
+export const createHaber = createAsyncThunk(
+  "haber/createHaber",
+  async (haber, thunkAPI) => {
+    try {
+      const resp = await axios.post(`api/v1/habers/`, haber);
+      return resp.data;
+    } catch (error) {
+      return checkForUnauthorizedResponse(error, thunkAPI);
+    }
+  }
+);
 
 const haberSlice = createSlice({
   name: "haber",
   initialState,
-  reducers: {},
+  reducers: {
+    handleChange: (state, { payload: { name, value } }) => {
+      state[name] = value;
+    },
+    clearForm: (state) => {
+      return initialState;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createHaber.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createHaber.fulfilled, (state, action) => {
+        state.isLoading = false;
+        toast.success("Haber başarıyla oluşturuldu.");
+      })
+      .addCase(createHaber.rejected, (state) => {
+        state.isLoading = false;
+      });
+  },
 });
 
-export const {} = haberSlice.actions;
+export const { handleChange, clearForm } = haberSlice.actions;
 export default haberSlice.reducer;
