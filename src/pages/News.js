5708b07bda7aa1401afa2ff332ad18e5d07c5eb0
Osman <114867365+osmansam@users.noreply.github.com>
2023-03-08 16:00:26 -0500

    haber are put in news and

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,17 +1,35 @@
 import { React, useEffect } from "react";
 import Section from "../components/Section";
-import { useSelector } from "react-redux";
-
+import HaberContainer from "../components/HaberContainer";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllHabers } from "../features/haber/haberSlice";
+import styled from "styled-components";
 const News = () => {
+  const dispatch = useDispatch();
   const { language } = useSelector((state) => state.bar);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+  const { habers } = useSelector((store) => store.haber);
+  useEffect(() => {
+    dispatch(getAllHabers());
+  }, []);
   return (
-    <main>
-      <Section props="news" />
-    </main>
+    <Wrapper>
+      <main>
+        <Section props="news" />
+        <div className="habers-container">
+          <HaberContainer habers={habers} />
+        </div>
+      </main>
+    </Wrapper>
   );
 };
+const Wrapper = styled.div`
+  .habers-container {
+    margin-top: 4rem;
+    width: 80%;
+  }
+`;
 
 export default News;
