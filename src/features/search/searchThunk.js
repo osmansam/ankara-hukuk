2fd297a660fb396fbd6e2b19718a571a47aadbad
Osman <114867365+osmansam@users.noreply.github.com>
2023-03-09 09:43:43 -0500

    haberler is set

--- src/features/search/searchThunk.js
+++ src/features/search/searchThunk.js
@@ -4,12 +4,15 @@ const baseURL = "/api/v1";
 
 export const getAllHabersThunk = async (_, thunkAPI) => {
   const { page, search, searchStatus, sort } = thunkAPI.getState().search;
+  console.log(search, sort, page, searchStatus);
   let url = `/habers?status=${searchStatus}&sort=${sort}&page=${page}`;
   if (search) {
     url = url + `&search=${search}`;
   }
+
   try {
     const resp = await axios.get(`${baseURL}/${url}`);
+    console.log(resp.data, "resp.data");
     return resp.data;
   } catch (error) {
     return checkForUnauthorizedResponse(error, thunkAPI);
